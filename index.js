1a19b0b9f862da65f1588e72daf6819909df4b20
Bradley Matjie <bradleysaint45@gmail.com>
2023-05-20 16:39:09 +0200

    added scrollbar

--- index.js
+++ index.js
@@ -1,22 +1,43 @@
 // shopping products
 let products = [
   {
-    id: 1,
+    id: 'js101d',
     name: 'Good old C++',
     Ingredients: 'white rum, sugar, lime juice, soda water and mint.',
     img: './images/c++.jpg',
-    price: 40,
+    price: 87.50
   },
   {
-    id: 2,
+    id: 'js203d',
     name: 'JS On The Rocks',
     Ingredients: 'tequila, lime and sugar',
     img: './images/js.jpg',
-    price: 40
+    price: 73.20
+  },
+  {
+    id: 'js306d',
+    name: 'Soft Function',
+    Ingredients: 'tequila, Lemon, Seasonal Tea',
+    img: './images/function.jpg',
+    price: 64.87
+  },
+  {
+    id: 'js404d',
+    name: '404 Aftermath',
+    Ingredients: 'Tangueray Gin, Proscecco, Lemon, Seasonal Tea',
+    img: './images/404.jpg',
+    price: 91.90
+  },
+  {
+    id: 'js503d',
+    name: 'Syntax Error',
+    Ingredients: 'tequila, lime sugar, Prescocco, sugar',
+    img: './images/yellow-cocktail.webp',
+    price: 95.50
   }
 ]
 
-
+// DISPLAYING THE PRODUCTS IN THE BROWSER
 products.map(product => {
   let item = `<div class="product">
     <img src=${product.img} id="img" alt="product img">
@@ -34,3 +55,9 @@ products.map(product => {
 
     document.querySelector(".products-container").innerHTML += item;
 })
+
+
+// ADD ITEMS TO CART
+function addToCart() {
+
+}
